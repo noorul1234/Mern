eb246e95d3795c21e01d1ebe3a307809d3b3f536
noorul <noorulabdeen1234@gmail.com>
2020-08-04 15:42:50 +0530

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,15 @@
+import { createStore,applyMiddleware, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk' 
+const midleware = [thunk];
+const reducer = combineReducers({
+  form: formReducer, // mounted under "form"
+});
+
+
+
+const store = (window.devToolsExtension ? window.devToolsExtension()(createStore): createStore)(reducer);
+
+
+
+export default store;
